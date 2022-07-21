685c50169b68b60a6b95eee239e1da59310fecba
LucioGhedo <lucioghedina@gmail.com>
2022-07-21 15:55:59 +0200

    fix

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pietro</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <style>
-        <?php include 'style/style.css'; ?>
-    </style>
+    <!-- <style>
+        < ?php include 'style/style.css'; ?>
+    </style> -->
 
     <?php 
         $badWords = $_GET['word'];
